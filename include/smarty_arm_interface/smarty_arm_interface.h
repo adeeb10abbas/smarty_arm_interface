d5d407ce17bfd7cf9001edf60fd328e7d357997d
adeeb10abbas <abbas.a@northeastern.edu>
2024-01-04 17:54:36 -0500

    almost there!

--- include/smarty_arm_interface/smarty_arm_interface.h
+++ include/smarty_arm_interface/smarty_arm_interface.h
@@ -12,7 +12,9 @@
 #include "std_msgs/msg/float64.hpp"
 #include "std_msgs/msg/bool.hpp"
 #include "geometry_msgs/msg/pose.hpp"
-#include "smarty_arm_interface/msg/ptipacket.hpp"
+// #include "smarty_arm_interface/msg/ptipacket.hpp"
+#include "smarty_arm_msg/msg/ptipacket.hpp"
+
 #include "rclcpp_lifecycle/lifecycle_node.hpp"
 #include "smarty_arm_interface/shm.h"
 /* C headers */
@@ -23,14 +25,13 @@ extern "C" {
 class SMARTY_ARM_Node : public rclcpp::Node {
 public:
     explicit SMARTY_ARM_Node(const std::string& name, Arm* arm, const std::string& type);
-
     // ~SMARTY_ARM_Node() override;
 
     void run();
 
 private:
-    rclcpp::Subscription<smarty_arm_interface::msg::Ptipacket>::SharedPtr smarty_arm_packet_sub;
-    rclcpp::Publisher<smarty_arm_interface::msg::Ptipacket>::SharedPtr smarty_arm_packet_pub;
+    rclcpp::Subscription<smarty_arm_msg::msg::Ptipacket>::SharedPtr smarty_arm_packet_sub;
+    rclcpp::Publisher<smarty_arm_msg::msg::Ptipacket>::SharedPtr smarty_arm_packet_pub;
     rclcpp::Publisher<geometry_msgs::msg::Pose>::SharedPtr smarty_arm_pose_pub;
 
     Arm* arm;
@@ -38,7 +39,7 @@ private:
 
     void publish_ptipacket();
     void publish_pose_state();
-    void ptipacket_callback(const smarty_arm_interface::msg::Ptipacket::SharedPtr msg);
+    void ptipacket_callback(const smarty_arm_msg::msg::Ptipacket::SharedPtr msg);
 
     // Replace ROS services with ROS2 services if needed
     // bool initSlave(std_srvs::srv::Empty::Request &req, std_srvs::srv::Empty::Response &res);
--- src/smarty_arm_interface.cpp
+++ src/smarty_arm_interface.cpp
@@ -4,42 +4,114 @@
 using namespace std;
 using namespace rclcpp;
 
-class SMARTY_ARM_Node : public rclcpp::Node {
-public:
-    SMARTY_ARM_Node(const std::string &name, Arm *armptr, const std::string &type);
 
-    void publish_ptipacket();
-    void publish_pose_state();
-    void ptipacket_callback(const smarty_arm_interface::Ptipacket::SharedPtr packet_msg);
-    void run();
+// SMARTY_ARM_Node::SMARTY_ARM_Node(const std::string &name, Arm *armptr, const std::string &type)
+//     : Node(name), arm(armptr), node_type(type) {
+//     // Initialization and topic subscriptions/publications
+//     // Adapt this section to use ROS2 Publisher and Subscriber initialization
+// }
+SMARTY_ARM_Node::SMARTY_ARM_Node(const std::string& name, Arm* arm, const std::string& type)
+: Node(name), arm(arm), node_type(type) {
+        // Initialize publishers and subscribers
+        if (node_type == "r") {
+            smarty_arm_packet_sub = this->create_subscription<smarty_arm_msg::msg::Ptipacket>(
+                "/pti_interface_right/pti_output", 1,
+                std::bind(&SMARTY_ARM_Node::ptipacket_callback, this, std::placeholders::_1));
+            smarty_arm_packet_pub = this->create_publisher<smarty_arm_msg::msg::Ptipacket>(
+                "/right_smarty_arm_output", 1);
+            smarty_arm_pose_pub = this->create_publisher<geometry_msgs::msg::Pose>(
+                "/right_arm_pose", 1);
+        } else if (node_type == "l") {
+            smarty_arm_packet_sub = this->create_subscription<smarty_arm_msg::msg::Ptipacket>(
+                "/pti_interface_left/pti_output", 1,
+                std::bind(&SMARTY_ARM_Node::ptipacket_callback, this, std::placeholders::_1));
+            smarty_arm_packet_pub = this->create_publisher<smarty_arm_msg::msg::Ptipacket>(
+                "/left_smarty_arm_output", 1);
+            smarty_arm_pose_pub = this->create_publisher<geometry_msgs::msg::Pose>(
+                "/left_arm_pose", 1);
+        }
 
-private:
-    std::string node_type;
-    rclcpp::Publisher<geometry_msgs::msg::Pose>::SharedPtr smarty_arm_pose_pub;
-    rclcpp::Publisher<smarty_arm_interface::Ptipacket>::SharedPtr smarty_arm_packet_pub;
-    rclcpp::Subscription<smarty_arm_interface::Ptipacket>::SharedPtr smarty_arm_packet_sub;
-    Arm *arm;
-    double origin_position[DOF / 2]; // Adjust DOF according to your application
-
-    // Additional members and methods as needed
-};
-
-SMARTY_ARM_Node::SMARTY_ARM_Node(const std::string &name, Arm *armptr, const std::string &type)
-    : Node(name), arm(armptr), node_type(type) {
-    // Initialization and topic subscriptions/publications
-    // Adapt this section to use ROS2 Publisher and Subscriber initialization
+        RCLCPP_INFO(this->get_logger(), "SMARTY_ARM_Node initialized");
 }
-
 void SMARTY_ARM_Node::publish_ptipacket() {
     // Publish logic, adapt for ROS2
+    /* Publish arm packet through ROS */
+
+    smarty_arm_msg::msg::Ptipacket packet_msg;
+
+    packet_msg.wave.resize(3);
+    packet_msg.test.resize(3);
+
+    pthread_mutex_lock(&arm->mutex);
+    for (int i = 0; i < 3; i++) {
+        packet_msg.wave[i] = arm->ptiPacket[i].wave_out;
+        packet_msg.test[i] = arm->ptiPacket[i].test;
+    }
+    packet_msg.position.x = arm->ee[0].pos;
+    packet_msg.position.y = arm->ee[1].pos;
+    packet_msg.position.z = arm->ee[2].pos;
+    packet_msg.angle.x = arm->ee[3].pos;
+    packet_msg.angle.y = arm->ee[4].pos;
+    packet_msg.angle.z = arm->ee[5].pos;
+    packet_msg.quat.w = arm->quat.w;
+    packet_msg.quat.x = arm->quat.x;
+    packet_msg.quat.y = arm->quat.y;
+    packet_msg.quat.z = arm->quat.z;
+    packet_msg.twist.linear.x = arm->ee[0].vel;
+    packet_msg.twist.linear.y = arm->ee[1].vel;
+    packet_msg.twist.linear.z = arm->ee[2].vel;
+    packet_msg.twist.angular.x = arm->ee[3].vel;
+    packet_msg.twist.angular.y = arm->ee[4].vel;
+    packet_msg.twist.angular.z = arm->ee[5].vel;
+    packet_msg.local_stamp = rclcpp::Clock().now().seconds();
+    packet_msg.remote_stamp = arm->ts.remote_time;
+    pthread_mutex_unlock(&arm->mutex);
+
+    smarty_arm_packet_pub->publish(packet_msg);
 }
+
+
 
 void SMARTY_ARM_Node::publish_pose_state() {
-    // Publish logic, adapt for ROS2
+    geometry_msgs::msg::Pose pose;
+    pose.position.x = arm->ee[0].pos;
+    pose.position.y = arm->ee[1].pos;
+    pose.position.z = arm->ee[2].pos;
+    pose.orientation.w = arm->quat.w;
+    pose.orientation.x = arm->quat.x;
+    pose.orientation.y = arm->quat.y;
+    pose.orientation.z = arm->quat.z;
+
+    smarty_arm_pose_pub->publish(pose);
+
 }
 
-void SMARTY_ARM_Node::ptipacket_callback(const smarty_arm_interface::Ptipacket::SharedPtr packet_msg) {
-    // Callback logic
+void SMARTY_ARM_Node::ptipacket_callback(const smarty_arm_msg::msg::Ptipacket::SharedPtr packet_msg) {
+    double delay_time;
+    pthread_mutex_lock(&arm->mutex);
+    for (int i = 0; i < 3; i++) {
+        arm->ptiPacket[i].wave_in = packet_msg->wave[i];
+    }
+
+    arm->ptiPacket[0].pos_in = packet_msg->position.x;
+    arm->ptiPacket[1].pos_in = packet_msg->position.y;
+    arm->ptiPacket[2].pos_in = packet_msg->position.z;
+
+    arm->ptiPacket[0].vel_in = packet_msg->twist.linear.x;
+    arm->ptiPacket[1].vel_in = packet_msg->twist.linear.y;
+    arm->ptiPacket[2].vel_in = packet_msg->twist.linear.z;
+
+    arm->ptiPacket[0].pos_d_in = packet_msg->position_d.x;
+    arm->ptiPacket[1].pos_d_in = packet_msg->position_d.y;
+    arm->ptiPacket[2].pos_d_in = packet_msg->position_d.z;
+
+    arm->ts.remote_time = packet_msg->local_stamp;
+    delay_time = (rclcpp::Clock().now().seconds() - packet_msg->remote_stamp) / 2.0;
+    arm->ts.delay_cycle = (int)(delay_time / 1e-3);
+    pthread_mutex_unlock(&arm->mutex);
+    
+    RCLCPP_INFO_THROTTLE(this->get_logger(), *this->get_clock(), 1, "Write into smarty arm memory, message delay: %lf", delay_time);
+
 }
 
 void SMARTY_ARM_Node::run() {
@@ -55,7 +127,7 @@ void SMARTY_ARM_Node::run() {
 int main(int argc, char **argv) {
     // rclcpp::init(argc, argv);
     // Arm initialization and ROS2 node creation
-    // auto node = std::make_shared<SMARTY_ARM_Node>("smarty_arm_interface", arm, std::string(argv[1]));
+    // auto node = std::make_shared<SMARTY_ARM_Node>("smarty_arm_msg", arm, std::string(argv[1]));
 
     // // Parameter handling setup if needed
 
@@ -65,3 +137,4 @@ int main(int argc, char **argv) {
     // rclcpp::shutdown();
     return 0;
 }
+
